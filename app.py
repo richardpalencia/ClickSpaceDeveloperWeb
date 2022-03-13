d8e4aabd57bfb92f068418938c351a57c96bcc85
richard <rpalencias86@gmail.com>
2022-03-13 16:50:49 -0600

    Integración de layout versión pc

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/identifica-tu-necesidad')
+def identifica():
+    return render_template('identifica.html')
+
+@app.route('/clientes')
+def clientes():
+    return render_template('clientes.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port='5001')
